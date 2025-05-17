3932144ab48d50277dddf29d76eedfcfffa67003
Leen <leen2005272@gmail.com>
2025-05-17 11:49:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -43,5 +43,8 @@ def predict():
 
     return render_template('services.html', results=top3)
 
-if __name__== '__main__':
-    app.run(debug=True,port=5000)
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
